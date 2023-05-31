71bc1ed248727e4840ced11abd292777cfd61a5a
Oluwaseun Williams <102968106+sheywilliams@users.noreply.github.com>
2023-05-31 11:56:36 +0100

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb = mysql.connector.connect(host = 'localhost', user = 'root', passwd = '1seunwilliams!' , database  = 'cms')
+mydb = mysql.connector.connect(host = 'localhost', user = 'root', passwd = '' , database  = 'cms')
 mycursor = mydb.cursor(buffered=True)
 
 # mycursor.execute("CREATE DATABASE cms")
